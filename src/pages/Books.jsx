0f48d1baa1b045d830dd0ec7e664641ec16acf08
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-12 11:24:41 -0400

    lazy load

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MovieState } from "../utils";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const Books = () => {
 	const [books, setBooks] = useState(MovieState);
 	useEffect(() => {
 		setBooks(MovieState);
 	}, []);
 
-	console.log(MovieState);
 	return (
 		<main id="books">
 			<div className="booksContainer">
 				{books.map((book) => (
-					<div className="bookContainer">
+					<div className="bookContainer" key={book.title}>
 						<Link to={`/books${book.url}`} state={{ book: book }}>
-							<img src={book.img} alt={book.title} />
+							<LazyLoadImage alt={book.title} src={book.img} />
 						</Link>
 					</div>
 				))}
